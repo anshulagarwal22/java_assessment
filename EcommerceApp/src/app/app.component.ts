b6f55852b8c38360817e44046ac3eddb55c07e52
anshul22 <anshagarwal4742@gmail.com>
2021-10-18 17:07:24 +0530

    Ecommerce Project

---
+++ EcommerceApp/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { ViewproductComponent } from './product/viewproduct/viewproduct.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My name is anshul';
+  //ViewproductComponent v=new ViewproductComponent();
+
+}
